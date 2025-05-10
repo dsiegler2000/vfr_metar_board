a60dd62dcf27172321b47a7bf0632dfbefe83d8f
dsiegler2000 <dsiegler2000@gmail.com>
2025-05-10 12:53:44 -0700

    prototyping gpio & sockets

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
 from flask import Flask, render_template
-
-import config
-import aviation_weather
 import airport_info as airports
+from flask_sock import Sock
+
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.debug = True
 
-@app.route('/')
+sock = Sock()
+sock.init_app(app)
+
+@app.route("/")
 def root():
-    # return aviation_weather.fetch_latest_metar("KSFO")
-    x = airports.get_airport_info("o69", check_cache=False)
-    return str(x)
+    return render_template("socket_test.html")
 
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
+@sock.route("/echo")
+def echo(ws):
+    while True:
+        data = ws.receive()
+        print(f"<<< {data}")
+        ws.send(data)
+
+from signal import pause
+import time
+
+# Pin Definitons:
+# pwmPin = 18 # Broadcom pin 18 (P1 pin 12)
+ledPin = 18
+butPin = 17
+
+from gpiozero import LED, Button
+from signal import pause
+
+led = LED(18)
+button = Button(17)
+
+button.when_pressed = led.on
+button.when_released = led.off
+
+
+
+    
--- readme.md
+++ readme.md
@@ -7,5 +7,9 @@ export FLASK_ENV=development
 flask --app app.py --debug run
 
 dsiegler@192.168.0.233
-pip freeze > requirements.txt 
+pip freeze > requirements.txt
+
+sudo apt-get install python-dev
+sudo apt install python-dev-is-python3
+sudo apt install python3-gpiozero
 ```
